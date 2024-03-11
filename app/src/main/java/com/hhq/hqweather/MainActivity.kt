898abb1a7ce8f3a4856d24ddbe9f21d1e1c00230
hehuiqi <hehuiqiwb942@hellobike.com>
2024-03-11 19:32:25 +0800

    项目基础架构配置

--- app/src/main/java/com/hhq/hqweather/MainActivity.kt
+++ app/src/main/java/com/hhq/hqweather/MainActivity.kt
@@ -2,10 +2,22 @@ package com.hhq.hqweather
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.hhq.hqweather.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val rootBinding:ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(rootBinding.root)
+        setup()
+    }
+    private fun setup() {
+        rootBinding.goBtn.setOnClickListener {
+            Toast.makeText(this,"hello",Toast.LENGTH_SHORT).show()
+        }
     }
 }
